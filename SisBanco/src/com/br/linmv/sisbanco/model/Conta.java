d63717bab257cc65dfd1d829d187f8975a9d4613
Luís Guilherme Nascimento Maruccio <luis.maruccio@gmail.com>
2018-05-28 22:38:09 -0300

    Alteração na Classe Conta

--- SisBanco/src/com/br/linmv/sisbanco/model/Conta.java
+++ SisBanco/src/com/br/linmv/sisbanco/model/Conta.java
@@ -4,4 +4,33 @@ public class Conta {
     private int codigo;
     private double saldo;
     private boolean inativo;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isInativo() {
+        return inativo;
+    }
+
+    public void setInativo(boolean inativo) {
+        this.inativo = inativo;
+    }
+    
+    
 }
+
+
+
